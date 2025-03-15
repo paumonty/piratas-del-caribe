87011e3dcbbee8c464f4bf24d863da55887005cd
paumonty <paumontanespla@gmail.com>
2025-03-15 12:08:00 +0100

    mistake

--- main.py
+++ main.py
@@ -11,25 +11,24 @@ disparos_maquina = set()
 
 
 while True:
-    print("\n🛡️ Tu tablero:")
+    print("\n Tu tablero:")
     mostrar_tablero(tablero_jugador)
 
-    print("\n🎯 Tablero de la máquina:")
-    mostrar_tablero(np.where(tablero_maquina == "O", "_", tablero_maquina))  # oculta barcos
+    print("\n Tablero de la máquina:")
+    mostrar_tablero(np.where(tablero_maquina == "O", "_", tablero_maquina))  
 
-    # Turno del jugador
+   
     casilla = pedir_casilla_al_usuario()
     disparar(casilla, tablero_maquina)
 
     if juego_terminado(tablero_maquina):
-        print("\n🎉 ¡Has ganado!")
+        print("\n ¡Has ganado!")
         break
 
-    # Turno de la máquina
     disparo_maquina(tablero_jugador, disparos_maquina)
 
     if juego_terminado(tablero_jugador):
-        print("\n💀 La máquina ha ganado...")
+        print("\n La máquina ha ganado...")
         break
 
     
